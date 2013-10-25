1b9e13b50e11e61b23fc0348310dfbbd7233e565
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2013-10-25 17:43:24 +0400

    Support native libraries search both in "<jar folder>" and "<jar folder>/libpty" folders

--- src/com/pty4j/util/PtyUtil.java
+++ src/com/pty4j/util/PtyUtil.java
@@ -3,6 +3,7 @@ package com.pty4j.util;
 import com.google.common.base.Function;
 import com.google.common.collect.Lists;
 import com.pty4j.windows.WinPty;
+import com.sun.jna.Platform;
 
 import java.io.File;
 import java.net.URLDecoder;
@@ -52,5 +53,59 @@ public class PtyUtil {
     Class aclass = WinPty.class;
 
     return getJarContainingFolder(aclass);
+  }
+
+  public static File resolveNativeLibrary() throws Exception {
+    File jarFolder = new File(getJarFolder());
+    File lib = resolveNativeLibrary(jarFolder);
+
+    lib = lib.exists() ? lib : resolveNativeLibrary(new File(jarFolder, "libpty"));
+
+    if (!lib.exists()) {
+      throw new IllegalStateException(String.format("Couldn't find %s, jar folder %s", lib.getName(),
+        jarFolder.getAbsolutePath()));
+    }
+
+    return lib;
+  }
+
+  public static File resolveNativeLibrary(File parent) {
+    File path = new File(parent, getPlatformFolder());
+
+    path = Platform.is64Bit() ? new File(path, "x86_64") : new File(path, "x86");
+
+    return new File(path, getNativeLibraryName());
+  }
+
+  private static String getPlatformFolder() {
+    String result;
+
+    if (Platform.isMac()) {
+      result = "macosx";
+    } else if (Platform.isWindows()) {
+      result = "win";
+    } else if (Platform.isLinux()) {
+      result = "linux";
+    } else {
+      throw new IllegalStateException("Platform " + Platform.getOSType() + " is not supported");
+    }
+
+    return result;
+  }
+
+  private static String getNativeLibraryName() {
+    String result;
+
+    if (Platform.isMac()) {
+      result = "libpty.dylib";
+    } else if (Platform.isWindows()) {
+      result = "libwinpty.dll";
+    } else if (Platform.isLinux()) {
+      result = "libpty.so";
+    } else {
+      throw new IllegalStateException("Platform " + Platform.getOSType() + " is not supported");
+    }
+
+    return result;
   }
 }
