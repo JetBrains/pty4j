09050bfb275fb92a3b08648f0a3e938a84bdbeda
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2018-08-15 12:00:19 +0300

    use JUnit4 syntax to simplify test

--- test/com/pty4j/FailedToLoadNativePtyLibTest.java
+++ test/com/pty4j/FailedToLoadNativePtyLibTest.java
@@ -1,22 +1,16 @@
 package com.pty4j;
 
-import junit.framework.TestCase;
-import org.junit.Assert;
+import org.junit.Test;
 import testData.RepeatTextWithTimeout;
 
 import java.io.IOException;
 
-public class FailedToLoadNativePtyLibTest extends TestCase {
-  public void testErrorMessage() {
+public class FailedToLoadNativePtyLibTest {
+
+  @Test(expected = IOException.class)
+  public void testErrorMessage() throws IOException {
     TestUtil.unsetLocalPtyLib();
     String[] cmd = TestUtil.getJavaCommand(RepeatTextWithTimeout.class, "2", "1000", "Hello, World");
-    boolean failed = false;
-    try {
-      PtyProcess.exec(cmd);
-    }
-    catch (IOException e) {
-      failed = true;
-    }
-    Assert.assertTrue("Library should fail to load unless 'PTY_LIB_FOLDER' system property is set", failed);
+    PtyProcess.exec(cmd);
   }
 }
